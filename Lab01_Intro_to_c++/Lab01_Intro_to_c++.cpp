1832ac91dd023c9d705b6d336f8598504078f7fc
Daniel Cleaver <23665045@stu.mmu.ac.uk>
2025-01-29 14:49:06 +0000

    Made addLevelMarks and most of outputMarks functions

--- Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
+++ Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <build/Student.h>
+#include <Lab01_Intro_to_c++/Student.h>
 
 int main()
 {
-    
+    Student* EW = new Student("Ellie", "Williams", 12345678, "Computer Science", 5);
+    (*EW).outputMarks();
     return 0;
 }
--- Lab01_Intro_to_c++/Student.h
+++ Lab01_Intro_to_c++/Student.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <iostream>
 using namespace std;
 
 class Student
@@ -19,5 +19,35 @@ public:
 		studentIDNumber = StudentIDNumber;
 		course = Course;
 		level = Level;
+	}
+
+	void addLevelMarks(int tempLevel, int smallMarks [4])
+	{
+		if (tempLevel == 4)
+		{
+			for (int i = 0; i < 3; i++) 
+			{
+				marks[i] = smallMarks[i];
+			}
+		}
+		if (tempLevel == 5)
+		{
+			for (int i = 4; i < 7; i++)
+			{
+				marks[i] = smallMarks[i];
+			}
+		}
+		if (tempLevel == 6)
+		{
+			for (int i = 8; i < 11; i++)
+			{
+				marks[i] = smallMarks[i];
+			}
+		}	
+	}
+	
+	void outputMarks() 
+	{
+		cout << firstName << " " << lastName << " " << "(" << studentIDNumber << ")" << endl;
 	}
 };
