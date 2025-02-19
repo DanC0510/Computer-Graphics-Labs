55426216e33482cc1d299cc62ce0dc116668c614
Daniel Cleaver <23665045@stu.mmu.ac.uk>
2025-02-19 13:36:32 +0000

    Arithmatic Operations on Vectors made

--- Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
+++ Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
@@ -19,5 +19,38 @@ int main() {
     std::cout << "a = " << a << std::endl;
     std::cout << "b = " << b << std::endl;
 
+    // Vector length
+    printf("\nVector length:\n");
+    printf("length(a) = %0.3f\n", glm::length(a));
+    printf("length(b) = %0.3f\n", glm::length(b));
+
+    // Normalising vectors
+    glm::vec3 aHat, bHat;
+    aHat = glm::normalize(a);
+    bHat = b / glm::length(b);
+
+    printf("\nNormalising vectors:\n");
+    std::cout << "aHat = " << aHat << std::endl;
+    std::cout << "bHat = " << bHat << std::endl;
+    printf("length(aHat) = %0.3f\n", glm::length(aHat));
+    printf("length(bHat) = %0.3f\n", glm::length(bHat));
+
+    // Arithmetic operations on vectors
+    printf("\nArithmetic operations on vectors:\n");
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "a - b = " << a - b << std::endl;
+
+    std::cout << "2a    = " << 2.0f * a << std::endl;
+    std::cout << "b / 3 = " << b / 3.0f << std::endl;
+
+    std::cout << "a * b = " << a * b << std::endl;
+
+    printf("\nDot and cross products:\n");
+    printf("a . b = %0.3f\n", glm::dot(a, b));
+
+    std::cout << "a x b = " << glm::cross(a, b) << std::endl;
+    printf("a . (a x b) = %0.3f\n", glm::dot(a, glm::cross(a, b)));
+    printf("b . (a x b) = %0.3f\n", glm::dot(b, glm::cross(a, b)));
+
     return 0;
 }
