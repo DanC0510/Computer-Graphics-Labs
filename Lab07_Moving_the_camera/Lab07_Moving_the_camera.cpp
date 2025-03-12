c418424f0969815e0d5951b1eab089672beeea00
Daniel Cleaver <146730101+DanC0510@users.noreply.github.com>
2025-03-12 14:20:28 +0000

    enabled back face culling

--- Lab07_Moving_the_camera/Lab07_Moving_the_camera.cpp
+++ Lab07_Moving_the_camera/Lab07_Moving_the_camera.cpp
@@ -75,6 +75,9 @@ int main( void )
     
     // Enable depth test
     glEnable(GL_DEPTH_TEST);
+
+    // Enable back face culling
+    glEnable(GL_CULL_FACE);
     
     // Ensure we can capture keyboard inputs
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
