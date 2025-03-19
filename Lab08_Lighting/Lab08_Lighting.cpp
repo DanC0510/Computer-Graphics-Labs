93de5dc9bd279d4994a3e6997ae20be6584701f5
Daniel Cleaver <23665045@stu.mmu.ac.uk>
2025-03-19 14:28:21 +0000

    drawing many teapots

--- Lab08_Lighting/Lab08_Lighting.cpp
+++ Lab08_Lighting/Lab08_Lighting.cpp
@@ -105,8 +105,53 @@ int main( void )
     teapot.addTexture("../assets/blue.bmp", "diffuse");
     
     // Use wireframe rendering (comment out to turn off)
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     
+    // Define teapot object lighting properties
+    teapot.kd = 0.7f;
+    teapot.ka = 0.2f;
+    teapot.ks = 1.0f;
+    teapot.Ns = 20.0f;
+
+    // Define light source properties
+    glm::vec3 lightPosition = glm::vec3(2.0f, 2.0f, 2.0f);
+    glm::vec3 lightColour = glm::vec3(1.0f, 1.0f, 1.0f);
+
+    float constant = 1.0f;
+    float linear = 0.1f;
+    float quadratic = 0.02f;
+
+    glUniform3fv(glGetUniformLocation(shaderID, "lightColour"), 1, &lightColour[0]);
+    glm::vec3 viewSpaceLightPosition = glm::vec3(camera.view * glm::vec4(lightPosition, 1.0f));
+    glUniform3fv(glGetUniformLocation(shaderID, "lightPosition"), 1, &viewSpaceLightPosition[0]);
+
+    // Teapot positions
+    glm::vec3 positions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -10.0f),
+        glm::vec3(-3.0f, -2.0f, -3.0f),
+        glm::vec3(-4.0f, -2.0f, -8.0f),
+        glm::vec3(2.0f,  2.0f, -6.0f),
+        glm::vec3(-4.0f,  3.0f, -8.0f),
+        glm::vec3(0.0f, -2.0f, -5.0f),
+        glm::vec3(4.0f,  2.0f, -4.0f),
+        glm::vec3(2.0f,  0.0f, -2.0f),
+        glm::vec3(-1.0f,  1.0f, -2.0f)
+    };
+
+    // Add teapots to objects vector
+    std::vector<Object> objects;
+    Object object;
+    object.name = "teapot";
+    for (unsigned int i = 0; i < 10; i++)
+    {
+        object.position = positions[i];
+        object.rotation = glm::vec3(1.0f, 1.0f, 1.0f);
+        object.scale = glm::vec3(0.75f, 0.75f, 0.75f);
+        object.angle = Maths::radians(20.0f * i);
+        objects.push_back(object);
+    }
+
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
@@ -125,6 +170,15 @@ int main( void )
         
         // Activate shader
         glUseProgram(shaderID);
+
+        // Send light source properties to the shader
+        glUniform1f(glGetUniformLocation(shaderID, "ka"), teapot.ka);
+        glUniform1f(glGetUniformLocation(shaderID, "ks"), teapot.ks);
+        glUniform1f(glGetUniformLocation(shaderID, "Ns"), teapot.Ns);
+        glUniform1f(glGetUniformLocation(shaderID, "kd"), teapot.kd);
+        glUniform1f(glGetUniformLocation(shaderID, "constant"), constant);
+        glUniform1f(glGetUniformLocation(shaderID, "linear"), linear);
+        glUniform1f(glGetUniformLocation(shaderID, "quadratic"), quadratic);
         
         // Calculate view and projection matrices
         camera.target = camera.eye + camera.front;
@@ -136,11 +190,24 @@ int main( void )
         glm::mat4 rotate;
         glm::mat4 model = translate * rotate * scale;
 
-        // Calculate the MVP matrix
-        glm::mat4 MVP = camera.projection * camera.view * model;
-        
-        // Send MVP matrix to the vertex shader
-        glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
+        // Loop through objects
+        for (int i = 0; i < static_cast<unsigned int>(objects.size()); i++)
+        {
+            // Calculate model matrix
+            glm::mat4 translate = Maths::translate(objects[i].position);
+            glm::mat4 scale = Maths::scale(objects[i].scale);
+            glm::mat4 rotate = Maths::rotate(objects[i].angle, objects[i].rotation);
+            glm::mat4 model = translate * rotate * scale;
+
+            // Send the MVP and MV matrices to the vertex shader
+            glm::mat4 MV = camera.view * model;
+            glm::mat4 MVP = camera.projection * MV;
+            glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
+            glUniformMatrix4fv(glGetUniformLocation(shaderID, "MV"), 1, GL_FALSE, &MV[0][0]);
+
+            // Draw the model
+            teapot.draw(shaderID);
+        }
         
         // Draw teapot
         teapot.draw(shaderID);
